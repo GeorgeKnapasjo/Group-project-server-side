33fec21e36822656c0cda347efa5aaa5325806b3
George.Knapasjo <george.knapasjo@studytafensw.edu.au>
2020-07-22 09:20:35 +1000

    Code updated

--- server/server.js
+++ server/server.js
@@ -16,11 +16,10 @@ const { v4: uuidv4 } = require('uuid');
         try {
             let project;
             if (!isNaN(req.body.postcode) && (req.body.postcode > 1999) && (req.body.postcode < 3000)) {
-                project ={...req.body}
-                // project = { id: uuidv4(), timeStamp: new Date().toISOString().slice(0, 17), status: "pending", voteCount: 0, ...req.body };
+                // project ={...req.body}
+                project = { id: uuidv4(), timeStamp: new Date().toISOString().slice(0, 17), status: "pending", voteCount: 0, ...req.body };
                 res.json(project);
-                // await storage.setItem(`project-${project.id}`, project);
-
+                await storage.setItem(`project-${project.id}`, project);
             }
             else{
                 throw new Error('wrong postcode')
@@ -28,15 +27,14 @@ const { v4: uuidv4 } = require('uuid');
         }
         catch (error) {
             res.json({status: 500, message: error.message});
-            // res.send(e.message)
-        }
+        };
+    });
 
-
-    })
+    
 
 
     server.listen(4000, () => {
         console.log('The server is listening on port 4000 http://localhost:4000');
-    })
+    });
 })();
 
