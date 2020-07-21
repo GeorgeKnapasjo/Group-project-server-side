f0302d0cb98105c055af9d74eb5424be6a6c4a1d
Annette Brand <Annette.Brand1@studytafensw.edu.au>
2020-07-21 12:15:20 +1000

    Adding listening port

--- server/server.js
+++ server/server.js
@@ -2,3 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const storage = require('node-persist');
+
+server.listen(4000, () =>{
+    console.log('The server is listening on port 4000 http://localhost:4000');
+})
