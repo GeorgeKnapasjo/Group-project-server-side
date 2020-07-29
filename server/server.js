c9db565eb226ebb3bef3084f9767cd80568f5a06
George.Knapasjo <george.knapasjo@studytafensw.edu.au>
2020-07-29 11:12:39 +1000

    submissions api fixed

--- server/server.js
+++ server/server.js
@@ -17,11 +17,11 @@ const server = express();
     server.get('/projects/approved', async (req, res) => {
         let projects = await storage.valuesWithKeyMatch(/project-/);
         let filteredResults = projects.filter(project => project.status == "approved")
-        res.json({status:200, data:filteredResults})
+        res.json({ status: 200, data: filteredResults })
     })
 
-    
-    
+
+
     server.get('/projects/pending', async (req, res) => {
         let projects = await storage.valuesWithKeyMatch(/project-/);
         let filteredResults = projects.filter(project => project.status == "pending")
@@ -29,7 +29,7 @@ const server = express();
     })
 
 
-    
+
     server.get('/projects/declined', async (req, res) => {
         let projects = await storage.valuesWithKeyMatch(/project-/);
         let filteredResults = projects.filter(project => project.status == "declined")
@@ -38,7 +38,7 @@ const server = express();
 
 
     //post handler for project submissions
-    
+
     server.post('/projects/submissions', async (req, res) => {
         try {
             let project = {
@@ -48,51 +48,52 @@ const server = express();
                 description: req.body.description,
                 title: req.body.title,
                 status: "pending",
+                grantAmount:req.body.grantAmount,
                 timeStamp: new Date().toISOString().slice(0, 17),
                 voteCount: 0
             }
             if (isNaN(project.postcode)) {
-                res.json({status:500, message:"Please enter a valid post code"})
+                res.json({ status: 500, message: "Please enter a valid post code" })
             }
-             else if(project.title.length > 50) {
-                 res.json({status:500, message:"Title must be below 50 characters"})
-             }
-             else if(project.description.length > 300) {
-                res.json({status:500, message:"Description must be below 300 characters"})
-    
-             }
-             else if (project.name.length > 50) {
-                res.json({status:500, message:"Name must be below 50 characters"})
-                 
-             }
-             else if(project.postcode < 1999 || project.postcode > 3000) {
-                res.json({status:500, message:"Please enter a valid post code"})
-    
-             }
-             else if(project.title == ''){
-                res.json({status:500, message:"Please enter a valid title"})
-             }
-    
-             else if (typeof project.name !== "string"){
-                res.json({status:500, message:"Please enter a valid name"})
-    
-             }
-             else if(project.postcode > 3000){
-                res.json({status:500, message:"Please enter a valid post code"})
-    
-             }
-             else if (project.name == "") {
-                res.json({status:500, message:"Please enter a valid name"})
-             }
-             else if (project.description == "") {
-                res.json({status:500, message:"Please enter a description"})
-             }
-             else if (typeof project.name !== "string") {
-                res.json({status:500, message:"Please enter a valid name"})
-             }
-             else if (typeof project.description !== "string") {
-                res.json({status:500, message:"Please enter a valid description"})
-    
+            else if (project.title.length > 50) {
+                res.json({ status: 500, message: "Title must be below 50 characters" })
+            }
+            else if (project.description.length > 300) {
+                res.json({ status: 500, message: "Description must be below 300 characters" })
+
+            }
+            else if (project.name.length > 50) {
+                res.json({ status: 500, message: "Name must be below 50 characters" })
+
+            }
+            else if (project.postcode < 1999 || project.postcode > 3000) {
+                res.json({ status: 500, message: "Please enter a valid post code" })
+
+            }
+            else if (project.title == '') {
+                res.json({ status: 500, message: "Please enter a valid title" })
+            }
+
+            else if (typeof project.name !== "string") {
+                res.json({ status: 500, message: "Please enter a valid name" })
+
+            }
+            else if (project.postcode > 3000) {
+                res.json({ status: 500, message: "Please enter a valid post code" })
+
+            }
+            else if (project.name == "") {
+                res.json({ status: 500, message: "Please enter a valid name" })
+            }
+            else if (project.description == "") {
+                res.json({ status: 500, message: "Please enter a description" })
+            }
+            else if (typeof project.name !== "string") {
+                res.json({ status: 500, message: "Please enter a valid name" })
+            }
+            else if (typeof project.description !== "string") {
+                res.json({ status: 500, message: "Please enter a valid description" })
+
             }
             else {
                 await storage.setItem(`project-${project.id}`, project);
@@ -103,7 +104,7 @@ const server = express();
             res.json({ status: 500, message: error.message });
         };
     });
-    
+
 
 
     //post handler for votes
@@ -112,14 +113,14 @@ const server = express();
         let project = await storage.getItem(`project-${toVote}`)
         ++project.voteCount
         await storage.updateItem(`project-${project.id}`, project)
-        res.json({status:200, data:project})
+        res.json({ status: 200, data: project })
     })
     //post handler for project searches
-    server.post('/projects/search', async (req, res) => {
+    server.get('/projects/search/:seachTerm', async (req, res) => {
 
         try {
-            if (req.body.search != "") {
-                let searchTerm = req.body.search.toLowerCase()
+            if (req.params.search != "") {
+                let searchTerm = req.params.search.toLowerCase()
                 let project = await storage.valuesWithKeyMatch(/project-/);
                 let filteredResults = project.filter(project => Object.keys(project).some(key => project[key].toString().toLowerCase().includes(searchTerm)))
                 let result = filteredResults.filter(p => p.status == "approved")
@@ -168,11 +169,20 @@ const server = express();
         };
     })
 
-// server.get('/projects/popular', async (req, res)=>{
-//     let projects = await storage.valuesWithKeyMatch(/project-/)
-//     let sortedProjects=projects.voteCount.sort((a, b) => (b-a))
-//     res.json(sortedProjects)
-// })
+    server.get('/projects/popular', async (req, res) => {
+        let projects = await storage.valuesWithKeyMatch(/project-/)
+        let filteredResults = projects.filter(project => project.status == "approved")
+        let sortedProjects = filteredResults.sort((a, b) => {
+            return (b.voteCount - a.voteCount)
+        })
+        if (sortedProjects.length < 10) {
+            sortedProjects.rank= 0
+            res.json(sortedProjects)
+        } else {
+            sortedProjects.length = 10
+            res.json(sortedProjects)
+        }
+    })
 
     server.listen(4000, () => {
         console.log('The server is listening on port 4000 http://localhost:4000');
